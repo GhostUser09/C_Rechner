7cc67d53f01ba36ee5759f11cf9d5cc46e1a00e2
GeistFan <ares.erwin.reisinger130209@gmail.com>
2022-11-13 01:31:53 +0100

    Update C_Rechner.c

--- C_Rechner.c
+++ C_Rechner.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <windows.h>
-//#include <conio.h>
+#include <windows.h>
+#include <conio.h>
+
+#define oe (unsigned char)148
+#define ae (unsigned char)132
+#define ue (unsigned char)129
+
+int a;
 
 float pi = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914564856692346034861045432664821339360726024914127372458700660631558817488152092096282925409171536436789259036001133053054882046652138414695194151160943305727036575959195309218611738193261179310511854807446237996274956735188575272489122793818301194912;
 
@@ -11,42 +17,53 @@ void taschenRechner()
     float b;
     float f;
     
-    printf("Gib eine Zahl für A an!\n");
+    printf("Gib eine Zahl fur A an!\n");
     scanf("%f", &a);
-    printf("Gib eine Zahl für B an!\n");
+    printf("Gib eine Zahl fur B an!\n");
     scanf("%f", &b);
-    printf("1 = +\n2 = -\n3 = *\n4 = /\n5 = Zurück\n");
+    printf("\n1 = +\n2 = -\n3 = *\n4 = /\n5 = Zur%cck\n", ue);
     scanf("%f", &f);
-    printf("A: %f\n", a);
+    printf("\nA: %f\n", a);
     printf("B: %f\n", b);
     
     if(f == 1)
     {
         printf("Das Ergebnis ist: %f\n", a + b);
+        getch();
+        system("cls");
+        main();
     }
     else if(f == 2)
     {
         printf("Das Ergebnis ist: %f\n", a - b);
+        getch();
+        system("cls");
+        main();
     }
     else if(f == 3)
     {
         printf("Das Ergebnis ist: %f\n", a * b);
+        getch();
+        system("cls");
+        main();
     }
     else if(f == 4)
     {
         printf("Das Ergebnis ist: %f\n", a / b);
+        getch();
+        system("cls");
+        main();
     }
     else if(f == 5)
     {
-        system("clear");
+        system("cls");
         main();
     }
     else
     {
         printf("Das ist keine Option!");
+        taschenRechner();
     }
-    
-    system("clear");
 }
 
 void kreisBerechnen()
@@ -55,11 +72,11 @@ void kreisBerechnen()
     
     printf("Gebe den Radius ein!\n");
     scanf("%f", &r);
-    printf("Radius: %f\n", r);
+    printf("\nRadius: %f\n", r);
     printf("Umfang: %f\n", 2 * r * pi);
-    printf("Fläche: %f\n", r * r * pi);
-    //getch();
-    system("clear");
+    printf("Fl%cche: %f\n", ae, r * r * pi);
+    getch();
+    system("cls");
 }
 
 void formelBerechnung()
@@ -69,16 +86,16 @@ void formelBerechnung()
     
     printf("Gebe einen Radius ein!\n");
     scanf("%f", &r);
-    printf("Gebe eine Höhe an!\n");
+    printf("\nGebe eine H%che an!\n", oe);
     scanf("%f", &h);
-    printf("Radius: %f\n", r);
-    printf("Höhe: %f\n", h);
-    printf("Kreisfläche: %f\n", r * r * pi);
+    printf("\nRadius: %f\n", r);
+    printf("H%che: %f\n", oe, h);
+    printf("Kreisfl%cche: %f\n", ae, r * r * pi);
     printf("Zylindervolumen: %f\n", r * r * pi * h);
     printf("Zylindermantel: %f\n", 2 * pi * r * h);
-    printf("Spherenfläche: %f\n", 4 * pi * r * r);
+    printf("Spherenfl%cche: %f\n", ae, 4 * pi * r * r);
     printf("Spherenvolumen: %f\n", 4/3 * pi * r * r * r);
-    //getch();
+    getch();
     system("clear");
     
 }
@@ -94,20 +111,42 @@ void basicFunction()
     printf("$$    $$   $$$$$$$  $$        $$$$$$    $$$$$$$$  $$    $$\n");
     printf("$$    $$  $$    $$  $$        $$   $$   $$        $$    $$\n");
     printf("$$    $$   $$$$$$$   $$$$$$$  $$    $$   $$$$$$$   $$$$$$$\n");
-    scanf("%f", basic);
+    printf("1. Taschenrechner\n2. Kreis berechnen\n3. Formelberechnung\n");
+    scanf("%f", &basic);
     
     if(basic == 1)
     {
-        system("clear");
-        taschenrechner;
+        system("cls");
+        taschenRechner();
     }
-    else if()
+    else if(basic == 2)
+    {
+        system("cls");
+        kreisBerechnen();
+    }
+    else if(basic == 3)
+    {
+        system("cls");
+        formelBerechnung();
+    }
+    else if(basic == 4)
+    {
+        system("cls");
+        main();
+    }
+    else
+    {
+        printf("N%c", oe);
+        getch();
+        system("cls");
+        main();
+    }
 }
 
 int main()
 {
-    //while(a < 1)
-    //{
+    while(a < 1)
+    {
         int auswahl;
         printf("$$$$$$$                       $$                                    \n");
         printf("$$    $$                      $$                                    \n");
@@ -117,37 +156,51 @@ int main()
         printf("$$    $$  $$        $$        $$    $$  $$    $$  $$        $$      \n");
         printf("$$    $$   $$$$$$$   $$$$$$$  $$    $$  $$    $$   $$$$$$$  $$      \n");
         printf("                                                                    \n");
-        printf("1. Taschenrechner\n2. Kreis berechnen\n3. Formelberechnung\n");
+        printf("1. Taschenrechner\n2. Kreis berechnen\n3. Formelberechnung\n4. Ausgang\n");
         scanf("%d", &auswahl);
         
         if(auswahl == 1)
         {
-            system("clear");
+            system("cls");
             taschenRechner();
         }
         else if(auswahl == 2)
         {
-            system("clear");
+            system("cls");
             kreisBerechnen();
         }
         else if(auswahl == 3)
         {
-            system("clear");
+            system("cls");
             formelBerechnung();
+        }
+        else if(auswahl == 4)
+        {
+            system("cls");
+            printf("Auf wieder sehen!");
+            getch();
+            return 0;
         }
         else if(auswahl == 1302)
         {
-            printf("Das ist keine Option!\n");
-            //getch();
-            system("clear");
+            printf("\nDas ist keine Option!\n");
+            getch();
+            system("cls");
             basicFunction();
         }
+        /*else if(auswahl == 5)
+        {
+            printf("Test");
+            printf("%c", ae);
+            printf("%c", oe);
+            printf("%c", ue);
+        }*/
         else
         {
-            printf("Das ist keine Option!");
-            //getch();
-            system("clear");
+            printf("\nDas ist keine Option!");
+            getch();
+            system("cls");
             main();
         }
-    //}
+    }
 }
